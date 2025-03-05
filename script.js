e6d4349707a092d5e5095ad3f68c391e45db6426
Soicrobbe <robbe.vanbiervliet@hotmail.com>
2025-03-05 14:56:19 +0100

    Added index, script and style

---
+++ script.js
@@ -0,0 +1,25 @@
+// // Smooth scrolling between sections
+// document.querySelectorAll('a').forEach(anchor => {
+//     anchor.addEventListener('click', function(e) {
+//         e.preventDefault();
+
+//         document.querySelector(this.getAttribute('href')).scrollIntoView({
+//             behavior: 'smooth'
+//         });
+//     });
+// });
+let toggled = false;
+function toggleBurger() {
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger-icon");
+    menu.classList.toggle("open");
+    // icon.classList.toggle("open");
+    toggled = !toggled;
+    if (toggled) {
+        icon.innerHTML = "&#x2716;";
+    } else {
+        icon.innerHTML = "&#9776;"
+    }
+    
+    
+}
